182c566fc231c92ec7dc2f44c214bcd8e4578bd5
Invigo <invigo.devel@gmail.com>
2020-02-07 00:56:00 +0200

    Update leap.cpp

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -1,21 +1,18 @@
 #include "leap.h"
 
-namespace leap {
+bool divBy400(int year) {
+    return (year % 400 == 0);
+}
 
-    bool divBy400(int year) {
-        return (year % 400 == 0);
-    }
+bool divBy100(int year) {
+    return (year % 100 == 0);
+}
 
-    bool divBy100(int year) {
-        return (year % 100 == 0);
-    }
+bool divBy004(int year) {
+    return (year % 4 == 0);
+}
 
-    bool divBy004(int year) {
-        return (year % 4 == 0);
-    }
-
-    bool is_leap_year(int year) {
-        return divBy400(year) || 
-              (divBy004(year) && !divBy100(year));
-    }
-}  // namespace leap
+bool leap::is_leap_year(int year) {
+    return divBy400(year) || 
+          (divBy004(year) && !divBy100(year));
+}
