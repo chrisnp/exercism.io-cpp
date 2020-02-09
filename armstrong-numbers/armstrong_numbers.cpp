b53653f2969acf3a11ba9812c391ee59ac38d1f9
Invigo <invigo.devel@gmail.com>
2020-02-09 02:48:16 +0200

    Update armstrong_numbers.cpp

--- armstrong-numbers/armstrong_numbers.cpp
+++ armstrong-numbers/armstrong_numbers.cpp
@@ -7,7 +7,7 @@ bool armstrong_numbers::is_armstrong_number(const int & num) {
     const string digits { to_string(num) };
 
     int num_digits { (int) digits.length() }, 
-        sum_digits_raised {};
+        sum_digits_raised {0};
 
     for (auto digit : digits) {
         sum_digits_raised += pow(digit - '0', num_digits); 
