b3c29e885517470efc51b51473d811880977163c
Invigo <invigo.devel@gmail.com>
2020-02-09 02:48:20 +0200

    Update reverse_string.cpp

--- reverse-string/reverse_string.cpp
+++ reverse-string/reverse_string.cpp
@@ -7,8 +7,8 @@ string reverse_string::reverse_string(const string &_str) {
     long unsigned int len { _str.length() };
     string str { _str };
     
-    return str.length() == 0 ? 
-           str               : 
-           reverse_string::reverse_string(str.substr(1, len - 1)) + str.at(0);
-
+    return len == 0 ? 
+           str      : 
+           reverse_string::reverse_string(str.substr(1, len - 1)) 
+                           + str.at(0);
 }
