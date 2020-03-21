d299e714ff2df1d1bfa255658a58e40cd948c9c1
Zero Delta <zerodeltahedge@gmail.com>
2020-03-21 22:58:32 +0200

    Update gigasecond.h

--- gigasecond/gigasecond.h
+++ gigasecond/gigasecond.h
@@ -1,8 +1,12 @@
-#if !defined(GIGASECOND_H)
-#define GIGASECOND_H
+#pragma once
+
+#include <cstdlib>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+using namespace boost::posix_time;
 
 namespace gigasecond {
 
-}  // namespace gigasecond
+ptime advance(ptime const &);
 
-#endif // GIGASECOND_H
+}  
