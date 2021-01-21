e56ba5a8d42f8133a97d26dbdcc5029253298e0d
Invigo <invigo.devel@gmail.com>
2021-01-21 16:34:53 +0200

    Update sum_of_multiples.h

--- sum-of-multiples/sum_of_multiples.h
+++ sum-of-multiples/sum_of_multiples.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
 namespace sum_of_multiples {
+
+using namespace std;
+
+typedef vector<int> divisors;
+
+int to(divisors factors, int up_to);
 
 }  // namespace sum_of_multiples
