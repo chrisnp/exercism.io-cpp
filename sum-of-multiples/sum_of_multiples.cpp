8b43e82d4197d9dbc431924b1a40ccdfdc3f16a8
Invigo <invigo.devel@gmail.com>
2021-01-21 16:46:09 +0200

    sum-of-multiples 1st iteration

--- sum-of-multiples/sum_of_multiples.cpp
+++ sum-of-multiples/sum_of_multiples.cpp
@@ -1,5 +1,27 @@
 #include "sum_of_multiples.h"
 
-namespace sum_of_multiples {
+using namespace std;
 
-}  // namespace sum_of_multiples
+bool multiple(long candidate, 
+              sum_of_multiples::divisors factors) {
+    for (auto x : factors) {
+        if (x != 0 && !(candidate % x))
+            return true;
+        else 
+            continue;
+    }
+    return false;
+}
+
+long sum_of_multiples::to(sum_of_multiples::divisors factors, 
+                          long up_to) {
+    vector<long> multiples {0};
+    for (long i {0}; i < up_to; ++i) {
+        if (multiple(i, factors)) {
+            multiples.push_back(i);
+        }
+    }
+    return accumulate(multiples.begin(), 
+                      multiples.end(),
+                      0);
+}
