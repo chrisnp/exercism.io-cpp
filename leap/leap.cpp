352dc76643be1002e0ef9b1e8b12ad104f8c3521
Invigo <invigo.devel@gmail.com>
2021-02-01 11:15:25 +0200

    Refactor leap.cpp

--- leap/leap.cpp
+++ leap/leap.cpp
@@ -1,20 +1,21 @@
 #include "leap.h"
 
 bool divBy400(int year) {
-    return (year % 400 == 0);
+    return year % 400 == 0;
 }
 
 bool divBy100(int year) {
-    return (year % 100 == 0);
+    return year % 100 == 0;
 }
 
 bool divBy004(int year) {
-    return (year % 4 == 0);
+    return year % 4 == 0;
 }
 
 bool leap::is_leap_year(int year) {
-    if (divBy100(year)) {
-        return divBy400(year);
-    }
-    return divBy004(year);
+    bool is_leap = 
+        divBy100(year) ? 
+        divBy400(year) : 
+        divBy004(year) ;
+    return is_leap;
 }
