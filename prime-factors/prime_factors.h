71fa01d6362a7e17efc758200ec22c2bf2569ddc
Invigo <invigo.devel@gmail.com>
2021-02-01 14:44:24 +0200

    Create prime_factors.h

---
+++ prime-factors/prime_factors.h
@@ -0,0 +1,14 @@
+#pragma once 
+
+#include <vector>
+#include <cstdint>
+
+namespace prime_factors {
+
+using namespace std;
+
+vector<int> of(uint64_t number);
+
+}  // namespace prime_factors
+
+#define EXERCISM_RUN_ALL_TESTS
