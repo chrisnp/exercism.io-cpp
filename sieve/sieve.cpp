b989fa89bf2ceee80f34ef14f084ff0347fdd3f1
Invigo <invigo.devel@gmail.com>
2021-05-31 18:41:45 +0300

    sieve.cpp 1st iteration, solution to sieve

--- sieve/sieve.cpp
+++ sieve/sieve.cpp
@@ -1,5 +1,19 @@
 #include "sieve.h"
 
-namespace sieve {
 
-}  // namespace sieve
+std::vector<int> sieve::primes(int limit) {
+
+    std::vector<bool> sieve(limit + 1, true);
+    std::vector<int> primes;
+
+    for (int candidate = 2; candidate <= limit; candidate++) {
+        if (!sieve[candidate]) {
+            continue;
+        }
+        primes.push_back(candidate);
+        for (int m = candidate; m <= limit; m += candidate) {
+            sieve[m] = false;
+        }
+    }   
+    return primes;
+}
