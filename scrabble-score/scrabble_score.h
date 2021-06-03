cf1762b785f41f00d0d45063a7d1793efe671756
Invigo <invigo.devel@gmail.com>
2021-06-03 23:31:17 +0300

    Update scrabble_score.h

--- scrabble-score/scrabble_score.h
+++ scrabble-score/scrabble_score.h
@@ -1,8 +1,25 @@
-#if !defined(SCRABBLE_SCORE_H)
-#define SCRABBLE_SCORE_H
+#pragma once
+
+#include <numeric>
+#include <string>
 
 namespace scrabble_score {
 
+static const int LETTER_SCORES[] 
+{ 
+  1, 3, 3, 2, 
+  1, 4, 2, 4, 
+  1, 8, 5, 1, 
+  3, 1, 1, 3, 
+  10, 
+  1, 1, 1, 1, 
+  4, 4, 8, 4, 
+  10
+};    
+
+int score(const std::string&);
+
 }  // namespace scrabble_score
 
-#endif // SCRABBLE_SCORE_H
+
+#define EXERCISM_RUN_ALL_TESTS
