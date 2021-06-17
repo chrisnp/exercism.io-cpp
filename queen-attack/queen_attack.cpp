ef5ddb07005965a4ea7ea4912afb9cb8b89e3e63
Zero Delta <zerodeltahedge@gmail.com>
2021-06-18 00:28:59 +0300

    Refactor queen_attack.cpp

--- queen-attack/queen_attack.cpp
+++ queen-attack/queen_attack.cpp
@@ -1,6 +1,7 @@
 #include "queen_attack.h"
 #include <sstream>
 
+
 queen_attack::chess_board::chess_board(const square &white, 
                                        const square &black) : 
                                        white_queen { white }, 
@@ -10,23 +11,21 @@ queen_attack::chess_board::chess_board(const square &white,
     } 
 }
 
-const square &queen_attack::chess_board::black() const noexcept {
-        return black_queen;
-    }
+const square &queen_attack::chess_board::black() const noexcept
+    { return black_queen; }
 
-const square &queen_attack::chess_board::white() const noexcept {
-        return white_queen;
-    }
+const square &queen_attack::chess_board::white() const noexcept
+    { return white_queen; }
 
 queen_attack::chess_board::operator std::string() const {
     std::stringstream board;
     for (int r = 0; r < 8; r++) {
         for (int f = 0; f < 8; f++) {
             if (f > 0) board << ' ';
-            square queen = {r, f};
-            if (queen == black_queen) board << 'B';
+            square p = {r, f};
+            if (p == black_queen) board << 'B';
             else 
-            if (queen == white_queen) board << 'W';
+            if (p == white_queen) board << 'W';
             else board << '_'; 
         }
         board << '\n';
@@ -35,9 +34,9 @@ queen_attack::chess_board::operator std::string() const {
 }
 
 bool queen_attack::chess_board::can_attack() const noexcept {
-    int delta_rank = abs(white_queen.first - black_queen.first);
-    int delta_file = abs(white_queen.second - black_queen.second);
+    int drank = abs(white_queen.first - black_queen.first);
+    int dfile = abs(white_queen.second - black_queen.second);
     
-    return delta_rank * delta_file == 0 ||
-           delta_rank / delta_file == 1;
+    return drank * dfile == 0 ||
+           drank / dfile == 1;
 }
