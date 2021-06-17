720a0fd3500bab2361a1a168048d76d4bc97bdb0
Invigo <invigo.devel@gmail.com>
2021-06-17 07:32:38 +0300

    Refactor queen_attack.h

--- queen-attack/queen_attack.h
+++ queen-attack/queen_attack.h
@@ -1,8 +1,29 @@
-#if !defined(QUEEN_ATTACK_H)
-#define QUEEN_ATTACK_H
+#pragma once
+
+#include <string>
+#include <utility>
+
+typedef std::pair<int, int> square;
 
 namespace queen_attack {
+    
+class chess_board {
+    
+public:
+    explicit chess_board(
+                const square &white = {0, 3}, 
+                const square &black = {7, 3}
+             );
 
+    const square &white() const noexcept;
+    const square &black() const noexcept;
+
+    explicit operator std::string() const;
+    bool can_attack() const noexcept;
+    
+private:
+    square white_queen, black_queen;
+};
 }  // namespace queen_attack
 
-#endif // QUEEN_ATTACK_H
+#define EXERCISM_RUN_ALL_TESTS
