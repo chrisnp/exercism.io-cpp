2640fff8c4af0ebe973173fa0ebc5e4ee82ef721
Invigo <invigo.devel@gmail.com>
2021-07-27 00:28:12 +0300

    Update grade_school.h

--- grade-school/grade_school.h
+++ grade-school/grade_school.h
@@ -1,8 +1,25 @@
-#if !defined(GRADE_SCHOOL_H)
-#define GRADE_SCHOOL_H
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
 
 namespace grade_school {
 
+typedef std::map<int, std::vector<std::string>> Roster;
+
+class school {
+
+public:
+    Roster roster() const;
+    void add(string student, int grade);
+    std::vector<std::string> grade(int grade) const;
+
+private:
+    
+
+};
+
 }  // namespace grade_school
 
-#endif // GRADE_SCHOOL_H
+#define EXERCISM_RUN_ALL_TESTS
