31ff8f49a9f8cd176202f8e88ac888b4e702cdd8
Invigo <invigo.devel@gmail.com>
2021-07-27 00:48:52 +0300

    Update grade_school.cpp

--- grade-school/grade_school.cpp
+++ grade-school/grade_school.cpp
@@ -1,5 +1,27 @@
 #include "grade_school.h"
 
-namespace grade_school {
+#include <algorithm>
+#include <stdexcept>
 
-}  // namespace grade_school
+using namespace std;
+
+grade_school::Roster grade_school::school::roster() const {
+    return school_;
+}
+
+void grade_school::school::add(string student, int grade) {
+    school_[grade].insert(school_[grade].end(), student);
+    sort(school_[grade].begin(), school_[grade].end());
+}
+
+vector<string> grade_school::school::grade(int grade) const {
+    // try {
+    //     return school_.at(grade);
+    // }
+    // catch (const std::logic_error &error) {
+    //     vector<string> empty {{}};
+    //     return empty;
+    // }
+}
+
+
