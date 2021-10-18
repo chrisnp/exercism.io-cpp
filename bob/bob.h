41a0c058919c0470645dd239fb17487d332c8c7c
Invigo <invigo.devel@gmail.com>
2021-10-19 01:59:01 +0300

    Refactor bob.h

--- bob/bob.h
+++ bob/bob.h
@@ -1,14 +1,19 @@
 #pragma once
 
 #include <string>
+#include <regex>
 
 namespace bob {
 
-    static const std::string RESPONSE_QUESTION { "Sure." };
-    static const std::string RESPONSE_SHOUT { "Whoa, chill out!" };
+    static const std::regex shout {"^[^a-z]*[A-Z][^a-z]*$"};
+    static const std::regex question {"\\?[^a-z]*$"};
+    static const std::regex silence {"^\\W*$"};
+
     static const std::string RESPONSE_EMPHATIC { "Calm down, I know what I'm doing!" };
-    static const std::string RESPONSE_SILENCE{ "Fine. Be that way!" };
-    static const std::string RESPONSE_DEFAULT{ "Whatever." };
+    static const std::string RESPONSE_SHOUTING { "Whoa, chill out!" };
+    static const std::string RESPONSE_QUESTION { "Sure." };
+    static const std::string RESPONSE_SILENCE  { "Fine. Be that way!" };
+    static const std::string RESPONSE_DEFAULT  { "Whatever." };
  
     std::string hey(const std::string &);
 
