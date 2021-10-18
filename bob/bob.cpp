3f0802df39d30b5570a835093e09cf275c2ad338
Invigo <invigo.devel@gmail.com>
2021-10-19 01:59:23 +0300

    Refactor bob.cpp

--- bob/bob.cpp
+++ bob/bob.cpp
@@ -1,25 +1,19 @@
 #include "bob.h"
-#include <regex>
 
 using namespace std;
 
-const regex shout {"^[[:lower:]]+[[:upper:]][^[:lower:]]+$"};
-const regex question {"\\?$"};
-const regex silence {"^\\W*$"};
-
 string bob::hey(const string &query) {
-    if (regex_search(query, shout)) {
-        if (regex_search(query, question)) {
-            return bob::RESPONSE_EMPHATIC;
-        } 
-        else {
-            return bob::RESPONSE_SHOUT;
-        }
+    if (regex_search(query, bob::shout) && 
+        regex_search(query, bob::question)) {
+        return bob::RESPONSE_EMPHATIC;
     }
-    if (regex_search(query, question)) { 
+    if (regex_search(query, bob::shout)) {
+        return bob::RESPONSE_SHOUTING;
+    }
+    if (regex_search(query, bob::question)) { 
         return bob::RESPONSE_QUESTION; 
     }
-    if (regex_search(query, silence)) { 
+    if (regex_search(query, bob::silence)) { 
         return bob::RESPONSE_SILENCE; 
     }
     return bob::RESPONSE_DEFAULT;
