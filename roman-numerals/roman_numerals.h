36b645068f2c5cb90ba957a371d30268b85c341a
Invigo <invigo.devel@gmail.com>
2021-11-17 18:17:41 +0200

    Refactor roman_numerals.h

--- roman-numerals/roman_numerals.h
+++ roman-numerals/roman_numerals.h
@@ -11,7 +11,7 @@ struct numeral_mapping {
 
 static const numeral_mapping arabic_to_roman[] {
     {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"},
-    {90, "XC"},  {50, "L"},   /*{49, "IL"}, */{40, "XL"},  {10, "X"},
+    {90, "XC"},  {50, "L"},   {40, "XL"},  {10, "X"},
     {9, "IX"},   {5, "V"},    {4, "IV"},  {1, "I"}
 };
 
