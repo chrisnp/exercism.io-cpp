1ef5ebeb1234873035f6af4107c017faa760b893
Invigo <invigo.devel@gmail.com>
2021-12-15 22:55:46 +0200

    Update anagram.cpp

--- anagram/anagram.cpp
+++ anagram/anagram.cpp
@@ -1,5 +1,31 @@
 #include "anagram.h"
+#include <algorithm>
 
-namespace anagram {
+using std::string;
+using std::vector;
 
-}  // namespace anagram
+anagram::anagram::anagram(const string word) {
+    this->subject = word;
+    transform(this->subject.begin(), 
+              this->subject.end(),
+              this->subject.begin(), ::tolower);
+    this->normal = this->subject;
+    std::sort(this->normal.begin(), 
+              this->normal.end());
+}
+
+vector<string> anagram::anagram::matches(vector<string> const &candidates) {
+    vector<string> matching {};
+    for( vector<string>::const_iterator candidate = candidates.begin();
+         candidate != candidates.end(); 
+         candidate ++ ) 
+    {
+        string word = *candidate;
+        transform(word.begin(), word.end(), word.begin(), ::tolower);
+        string sortedLetters = word;
+        sort(sortedLetters.begin(), sortedLetters.end());
+        if (sortedLetters == this->normal && word != this->subject)
+            matching.push_back(*candidate);
+    }
+    return matching;
+}
