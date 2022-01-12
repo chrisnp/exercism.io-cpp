be853668148def6e0c07135a19d02f07609341cb
Invigo <invigo.devel@gmail.com>
2022-01-12 18:44:51 +0200

    Update trinary.cpp

--- trinary/trinary.cpp
+++ trinary/trinary.cpp
@@ -1,20 +1,21 @@
 #include "trinary.h"
 #include <algorithm>
 
-bool valid(std::string const &trinary) {
+bool valid(std::string const &trinary) noexcept {
     return std::all_of(trinary.begin(), trinary.end(), [](char d) {
-        return ('0' <= d) && (d <= '2');
+        // return ('0' <= d) && (d <= '2');
+        return (d == std::clamp(d, '0', '2'));
     });
 }
 
 int trinary::to_decimal(const std::string &trinary) noexcept {
     if (!valid(trinary)) return 0;
-    int decimal, dval {0};
+    int decimal {0};
     for (auto d = trinary.begin(); d < trinary.end(); ++d) {
         decimal += decimal << 1;
-        dval = *d - '0';
-        if (dval > 2) continue;
-        decimal += dval;
+        // dval = *d - '0';
+        // if (dval > 2) continue;
+        decimal += *d - '0';
     }
     return decimal;
 }
