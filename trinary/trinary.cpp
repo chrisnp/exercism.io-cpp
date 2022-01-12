a569b2d668f64ca3993ebb5e954114519cd5e163
Invigo <invigo.devel@gmail.com>
2022-01-12 18:04:26 +0200

    Refactor trinary.cpp

--- trinary/trinary.cpp
+++ trinary/trinary.cpp
@@ -1,5 +1,20 @@
 #include "trinary.h"
+#include <algorithm>
 
-namespace trinary {
+bool valid(std::string const &trinary) {
+    return std::all_of(trinary.begin(), trinary.end(), [](char d) {
+        return ('0' <= d) && (d <= '2');
+    });
+}
 
-}  // namespace trinary
+int trinary::to_decimal(const std::string &trinary) noexcept {
+    if (!valid(trinary)) return 0;
+    int decimal, dval {0};
+    for (auto d = trinary.begin(); d < trinary.end(); ++d) {
+        decimal += decimal << 1;
+        dval = *d - '0';
+        if (dval > 2) continue;
+        decimal += dval;
+    }
+    return decimal;
+}
