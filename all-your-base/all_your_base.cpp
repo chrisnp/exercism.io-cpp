017f481d93ddfdbd04939280005614718783d1ac
Invigo <invigo.devel@gmail.com>
2022-01-24 23:50:54 +0200

    1st passing iteration - all-your-base

--- all-your-base/all_your_base.cpp
+++ all-your-base/all_your_base.cpp
@@ -2,8 +2,29 @@
 
 #include <algorithm>
 #include <stdexcept>
+#include <cmath>
 
-use namespace std;
+using namespace std;
 
-vector<int> all_your_base::convert(int in_base, vector<int> &num, int out_base) {
+vector<unsigned int> all_your_base::convert(unsigned int in_base, 
+                                            const vector<unsigned int> &in_representation, 
+                                            unsigned int out_base) {
+    vector<unsigned int> out_representation;
+    if (in_base < 2 || out_base < 2) 
+        throw invalid_argument("bases must be at least 2");
+    if (in_representation.size() == 0)
+        return out_representation;
+    int sum {0}; int pos {0};
+    for (auto digit = in_representation.rbegin(); digit != in_representation.rend(); ++digit) {
+        if (*digit >= in_base) 
+            throw invalid_argument("invalid digit");
+        sum += *digit * pow(in_base, pos);
+        ++pos;
+    }
+    while (sum >= 1) {
+        out_representation.push_back(sum % out_base);
+        sum /= out_base;
+    } 
+    reverse(out_representation.begin(), out_representation.end());
+    return out_representation;
 }
