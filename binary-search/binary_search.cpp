a96e70941f59959782465174551801ac01b0cddb
Invigo <invigo.devel@gmail.com>
2022-02-21 21:23:20 +0200

    Update binary_search.cpp

--- binary-search/binary_search.cpp
+++ binary-search/binary_search.cpp
@@ -1,5 +1,22 @@
 #include "binary_search.h"
+#include <cassert>
+#include <algorithm>
+#include <stdexcept>
 
-namespace binary_search {
+using namespace std;
 
-}  // namespace binary_search
+int binary_search::find( vector<int> haystaq, int needle ) {
+    assert( is_sorted( haystaq.cbegin(), haystaq.cend() ) );
+
+    int lo = 0, hi = haystaq.size() - 1;
+    while( lo <= hi ) {
+        int mid = (lo + hi) >> 1;
+        if( haystaq.at(mid) == needle )
+            return mid;
+        else if( haystaq.at(mid) < needle )
+            lo = mid + 1;
+        else 
+            hi = mid - 1;
+    }
+    throw domain_error("not found");
+}
