3703caeaa2c916b1c2d53042d2f703f618d7178c
Invigo <invigo.devel@gmail.com>
2022-02-22 23:46:27 +0200

    Refactor luhn.cpp

--- luhn/luhn.cpp
+++ luhn/luhn.cpp
@@ -1,5 +1,20 @@
 #include "luhn.h"
+#include <algorithm>
 
-namespace luhn {
+using namespace std;
 
-}  // namespace luhn
+bool luhn::valid( string num ) {
+    num.erase(remove(num.begin(), num.end(), ' '), num.end());
+    if (num.length() < 2) return false;
+    int csum { 0 };
+    reverse(num.begin(), num.end());
+    for (unsigned int i = 0; i < num.length(); ++i) {
+        if (isdigit(num[i])) {
+            int d = int( num[i] ) - '0';
+            d = i % 2 != 0 ? (d > 4 ? (d * 2) - 9 : d * 2) : d;
+            csum += d;
+        }
+        else return false;
+    }
+    return !(csum % 10);
+}
