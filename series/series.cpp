d76bd2f9d24153db95e8f8d73ad8d7d1216b814e
Invigo <invigo.devel@gmail.com>
2022-03-15 19:46:41 +0200

    Refactor series.cpp

--- series/series.cpp
+++ series/series.cpp
@@ -5,23 +5,23 @@ using namespace std;
 
 vector<int> series::digits(string const &input) {
     vector<int> digits {};
-    for (auto &c: input) {
+    for( auto &c: input ) {
         int d = c - '0';
-        if (d < 0 || d > 9) 
+        if( d < 0 || d > 9 ) 
             throw domain_error("invalid digit");
         digits.push_back(d);
     }
     return digits;
 }
 
-vector<vector<int>> series::slice(string const &input, size_t size) {
+vector<vector<int>> series::slice(string const &input, size_t slice_size) {
     vector<int> digits = series::digits(input);
     size_t input_size = digits.size();
-    if (input_size < size)
+    if( input_size < slice_size )
         throw domain_error("cannot slice shorter string");
     vector<vector<int>> slices {};
-    for (size_t i = 0; i <= input.size() - size; ++i) {
-        slices.push_back(series::digits(input.substr(i, size)));
+    for( size_t i = 0; i <= input.size() - slice_size; ++i ) {
+        slices.push_back(series::digits(input.substr(i, slice_size)));
     }  
     return slices;
 }
