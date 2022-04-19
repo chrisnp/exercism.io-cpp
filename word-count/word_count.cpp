4830e4f3c899f3a2f1775b44dc011a7115db3e20
Invigo <invigo.devel@gmail.com>
2022-04-19 21:27:14 +0300

    Update word_count.cpp

--- word-count/word_count.cpp
+++ word-count/word_count.cpp
@@ -1,5 +1,28 @@
 #include "word_count.h"
+#include <vector>
 
-namespace word_count {
+using namespace std;
 
-}  // namespace word_count
+bool delim(const char &c) {
+    return c == ' ' || c == '\n' || c == ',';
+}
+
+const map<string, int> word_count::words(const string &text) {
+    map<string, int> wordcount {};
+    vector<char> wordchars {};
+    for ( unsigned int i = 0; i < text.length(); i++ ) {
+        auto &c = text[i];
+        if ( delim(c) && !wordchars.empty() ) {
+            wordcount[string(wordchars.begin(), wordchars.end())] += 1;
+            wordchars = vector<char>();
+        } 
+        else if ( (c == '\'' && isalnum(text[i - 1]) && isalnum(text[i + 1])) 
+                  || isalnum(c) ) {
+            wordchars.push_back(tolower(c));
+        }
+        else continue;
+    }
+    if ( !wordchars.empty()) 
+        wordcount[string(wordchars.begin(), wordchars.end())] += 1;
+    return wordcount;
+}
