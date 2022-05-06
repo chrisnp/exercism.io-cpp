12b8ebbf284c41eddc7c3a0c587c734005634653
Invigo <invigo.devel@gmail.com>
2022-05-06 23:52:36 +0300

    Update atbash_cipher.cpp

--- atbash-cipher/atbash_cipher.cpp
+++ atbash-cipher/atbash_cipher.cpp
@@ -5,9 +5,10 @@ using namespace std;
 string atbash_cipher::encode(const string &plain) {
     string cipher {};
     for( char c: plain) {
+        c = tolower(c);
         bool digit  = '0' <= c && c <= '9';
         bool letter = 'a' <= c && c <= 'z';
-        
+        if( !digit || !letter ) continue;
     }
 }
 
