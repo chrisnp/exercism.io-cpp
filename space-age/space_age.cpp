710cc3bbfdaaf2e903cf54333be0d008d735dfd5
Invigo <invigo.devel@gmail.com>
2022-05-09 01:21:23 +0300

    Refactor space-age\space_age.cpp

--- space-age/space_age.cpp
+++ space-age/space_age.cpp
@@ -11,7 +11,7 @@ enum planet {
         NEPTUNE
 };
 
-const double Orbital_Period[] {
+static constexpr double Orbital_Period[] {
         0.2408467, 
         0.61519726, 
         1.0000000,
@@ -24,7 +24,7 @@ const double Orbital_Period[] {
 
 const uint64_t Earth_Year = 31557600;
 
-space_age::space_age::space_age(uint_fast64_t _seconds) {
+space_age::space_age::space_age( const uint_fast64_t _seconds ) {
         age = _seconds; 
 }
 
@@ -32,34 +32,34 @@ uint_fast64_t space_age::space_age::seconds() const {
     return age;
 }
 
-float space_age::space_age::on_mercury() const {
+float space_age::space_age::on_mercury() const noexcept {
     return age / (Orbital_Period[MERCURY] * Earth_Year);
 }
 
-float space_age::space_age::on_venus() const {
+float space_age::space_age::on_venus()   const noexcept {
     return age / (Orbital_Period[VENUS] * Earth_Year);
 }
 
-float space_age::space_age::on_earth() const {
+float space_age::space_age::on_earth()   const noexcept {
     return age / (Orbital_Period[EARTH] * Earth_Year);
 }
 
-float space_age::space_age::on_mars() const {
+float space_age::space_age::on_mars()    const noexcept {
     return age / (Orbital_Period[MARS] * Earth_Year);
 }
 
-float space_age::space_age::on_jupiter() const {
+float space_age::space_age::on_jupiter() const noexcept {
     return age / (Orbital_Period[JUPITER] * Earth_Year);
 }
 
-float space_age::space_age::on_saturn() const {
+float space_age::space_age::on_saturn()  const noexcept {
     return age / (Orbital_Period[SATURN] * Earth_Year);
 }
 
-float space_age::space_age::on_uranus() const {
+float space_age::space_age::on_uranus()  const noexcept {
     return age / (Orbital_Period[URANUS] * Earth_Year);
 }
 
-float space_age::space_age::on_neptune() const {
+float space_age::space_age::on_neptune() const noexcept {
     return age / (Orbital_Period[NEPTUNE] * Earth_Year);
 }
