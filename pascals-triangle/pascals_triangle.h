283d58f74e9d804e41b3e422006b86afcd9f1731
Invigo <invigo.devel@gmail.com>
2022-07-19 03:49:50 +0300

    Update pascals_triangle.h

--- pascals-triangle/pascals_triangle.h
+++ pascals-triangle/pascals_triangle.h
@@ -1,8 +1,20 @@
-#if !defined(PASCALS_TRIANGLE_H)
-#define PASCALS_TRIANGLE_H
+#pragma once
+#include <vector>
 
 namespace pascals_triangle {
 
-}  // namespace pascals_triangle
+constexpr factorial (int n)  noexcept { 
+    return n == 0 ? 1 : n * factorial(n - 1);
+}
+constexpr binomial (int n, int k) noexcept {
+    return factorial(n) / (factorial(k) * factorial(n - k));
+}
+using ptriangle_t = std::vector<std::vector<int>>;
+const ptriangle_t generate_rows(int);
 
-#endif // PASCALS_TRIANGLE_H
+} // namespace pascals_triangle 
+
+
+#ifndef EXERCISM_RUN_ALL_TESTS
+#define EXERCISM_RUN_ALL_TESTS
+#endif
