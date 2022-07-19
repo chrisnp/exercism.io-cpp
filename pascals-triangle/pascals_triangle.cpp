b2ebb4220a2ee2d431dbfff5fe58b9124ace75cc
Invigo <invigo.devel@gmail.com>
2022-07-19 03:51:03 +0300

    pascals_triangle.cpp -> passing all tests

--- pascals-triangle/pascals_triangle.cpp
+++ pascals-triangle/pascals_triangle.cpp
@@ -1,5 +1,16 @@
 #include "pascals_triangle.h"
 
-namespace pascals_triangle {
+using namespace std;
 
-}  // namespace pascals_triangle
+const pascals_triangle::ptriangle_t pascals_triangle::generate_rows(int n) {
+
+    ptriangle_t ptriangle {};
+    for (int i = 0; i < n; i++) {
+        vector<int> row {};
+        for (int j = 0; j <= i; j++) {
+            row.push_back(pascals_triangle::binomial(i, j));
+        }
+        ptriangle.push_back(row);
+    }
+    return ptriangle;
+}
