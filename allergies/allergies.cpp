873fc8f10261e54d5253ff93f9b2b18a95cc68d3
Invigo <invigo.devel@gmail.com>
2022-09-21 01:19:09 +0300

    Update allergies.cpp

--- allergies/allergies.cpp
+++ allergies/allergies.cpp
@@ -1,17 +1,15 @@
 #include "allergies.h"
 
-using namespace std;
-
 allergies::allergy_test::allergy_test(unsigned short score) {
     score &= 0xff;
     for (int i = 0; i < static_cast<int>(ALLERGENS.size()); i++) 
         if (1 << i & score) allergies.insert(ALLERGENS.at(i));
 }
 
-bool allergies::allergy_test::is_allergic_to(const string &allergen) {
+bool allergies::allergy_test::is_allergic_to(const std::string &allergen) {
     return allergies.count(allergen) != 0;
 }
 
-unordered_set<string> allergies::allergy_test::get_allergies() const {
+std::unordered_set<std::string> allergies::allergy_test::get_allergies() const {
     return allergies::allergy_test::allergies;
 }
