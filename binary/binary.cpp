4d60a3020d84a788e211c113e783c970dd0d73fa
Invigo <invigo.devel@gmail.com>
2022-09-22 18:46:40 +0300

    Update binary.cpp

--- binary/binary.cpp
+++ binary/binary.cpp
@@ -5,11 +5,10 @@ using namespace std;
 
 long binary::convert (string binstr) {
     for (auto& d: binstr) {
-        if (!isdigit(d)) 
-            return 0;
+        if (!isdigit(d)) return 0;
     }
-    auto binary = stoll(binstr); 
-    long decimal = 0, pos = 0;
+    auto binary {stoll(binstr)}; 
+    long decimal {0}, pos {0};
     while (binary != 0) {
         decimal += (binary % 10) * pow(2, pos);
         pos++;
