4980328b40d3ad6addaa75450197d48d8cfb2af6
Zero Delta <zerodeltahedge@gmail.com>
2022-09-23 03:49:03 +0300

    Refactor difference_of_squares.h

--- difference-of-squares/difference_of_squares.h
+++ difference-of-squares/difference_of_squares.h
@@ -4,8 +4,13 @@
 
 namespace difference_of_squares {
 
-long int square_of_sum(const int& _n);
-long int sum_of_squares(const int& _n);
-long int difference(const int& _n);
+constexpr int ipow(int base, int exp) {
+    return exp == 1 ? base
+    : ipow(base, (exp >> 1) + (exp & 1)) * ipow(base, exp >> 1);
+}
+
+long int square_of_sum(const int&);
+long int sum_of_squares(const int&);
+long int difference(const int&);
 
 }
