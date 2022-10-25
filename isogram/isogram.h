a68ec9a61e517db183720b30359ccfee617caa90
Invigo <invigo.devel@gmail.com>
2022-10-25 23:04:14 +0300

    Refactor isogram.h

--- isogram/isogram.h
+++ isogram/isogram.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#define EXERCISM_RUN_ALL_TESTS
-
 #include <string>
 
 using namespace std;
 namespace isogram {
 
-    bool is_isogram(const string &);
+bool is_isogram(const string &);
 
 }  
+
+#ifndef EXERCISM_RUN_ALL_TESTS
+#define EXERCISM_RUN_ALL_TESTS
+#endif
