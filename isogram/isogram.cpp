208fcb93598c656843a58e9984eb0ce98164750b
Invigo <invigo.devel@gmail.com>
2022-10-25 23:04:18 +0300

    Update isogram.cpp

--- isogram/isogram.cpp
+++ isogram/isogram.cpp
@@ -1,19 +1,14 @@
 #include "isogram.h"
+
 #include <set>
 
 bool isogram::is_isogram(const string &word) {
-
     set<char> isogram {};
     
     for(char ch : word) {
-
         if (!isalpha(ch)) continue;
-        
         ch = tolower(ch);
-
-        if (isogram.count(ch) > 0) 
-            return false;
-        
+        if (isogram.count(ch) > 0) return false;
         isogram.insert(ch);
     }
     return true;
