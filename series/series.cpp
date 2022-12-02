521351af4a7c6f3121e906d7c5396aaf089999eb
Invigo <invigo.devel@gmail.com>
2022-12-02 19:27:07 +0200

    Update domain_error in series.cpp

--- series/series.cpp
+++ series/series.cpp
@@ -8,7 +8,7 @@ vector<string> series::slice(string const &input, uint32_t slice_size) {
         throw domain_error("enigmatic error for not actually slicing");
     uint32_t input_size = input.length();
     if ( input_size < slice_size ) 
-        throw domain_error("cannot slice a string shorter than the slice");
+        throw domain_error("cannot slice a string shorter than the slicesize");
     vector<string> slices {};
     for ( uint32_t i = 0; i <= input_size - slice_size; ++i ) {
         slices.push_back(input.substr(i, slice_size));
