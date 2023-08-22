02f92593efeca97140c45f63e945a2bd769b1bc9
Invigo <invigo.devel@gmail.com>
2023-08-22 15:33:52 +0300

    Update lasagna.cpp

--- lasagna/lasagna.cpp
+++ lasagna/lasagna.cpp
@@ -1,31 +1,18 @@
-// ovenTime returns the amount in minutes that the lasagna should stay in the
-// oven.
+const int BAKING_TIME = 40;
+const int LAYER_PREP = 2;
+
 int ovenTime() {
-    // TODO: Return the correct time.
-    return 0;
+    return BAKING_TIME;
 }
 
-/* remainingOvenTime returns the remaining
-   minutes based on the actual minutes already in the oven.
-*/
 int remainingOvenTime(int actualMinutesInOven) {
-    // TODO: Calculate and return the remaining in the oven based on the time
-    // the lasagna has already been there.
-    return 0;
+    return BAKING_TIME - actualMinutesInOven;
 }
 
-/* preparationTime returns an estimate of the preparation time based on the
-   number of layers and the necessary time per layer.
-*/
 int preparationTime(int numberOfLayers) {
-    // TODO: Calculate and return the preparation time with the
-    // `numberOfLayers`.
-    return 0;
+    return LAYER_PREP * numberOfLayers; 
 }
 
-// elapsedTime calculates the total time spent to create and bake the lasagna so
-// far.
 int elapsedTime(int numberOfLayers, int actualMinutesInOven) {
-    // TODO: Calculate and return the total time so far.
-    return 0;
+    return preparationTime(numberOfLayers) + actualMinutesInOven;
 }
