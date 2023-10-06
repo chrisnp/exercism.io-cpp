e58969b138232da068f12aa841df0b389a59ea02
Invigo <invigo.devel@gmail.com>
2023-10-06 22:32:49 +0300

    Refactor grade_school.h

--- grade-school/grade_school.h
+++ grade-school/grade_school.h
@@ -6,18 +6,16 @@
 
 namespace grade_school {
 
-typedef std::map<int, std::vector<std::string>> Roster;
+using Roster = std::map<int, std::vector<std::string>>;
 
 class school {
-
 public:
     Roster roster() const;
     void add(std::string, int);
     std::vector<std::string> grade(int) const;
 private:
     Roster school_;
-};
-
+}; // class school
 }  // namespace grade_school
 
 #ifndef EXERCISM_RUN_ALL_TESTS
