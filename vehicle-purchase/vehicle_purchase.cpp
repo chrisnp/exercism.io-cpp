9f1860d47f0afdb03f63e43ec87f27b88edfe625
Invigo <invigo.devel@gmail.com>
2023-11-01 02:29:57 +0200

    vehicle_purchase.cpp passing all tests

--- vehicle-purchase/vehicle_purchase.cpp
+++ vehicle-purchase/vehicle_purchase.cpp
@@ -1,23 +1,25 @@
 #include "vehicle_purchase.h"
 
 namespace vehicle_purchase {
-
-    // needs_license determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
-    bool needs_license(std::string kind){
-        // TODO: Return true if you need a license for that kind of vehicle.
-        return false;
+    // using string = std::string;
+    // needs_license determines whether a license is needed to drive a type of vehicle. 
+    // Only "car" and "truck" require a license.
+    bool needs_license(const string &kind) noexcept {
+        return !(!(kind == "car") && !(kind == "truck"));
     }
-
-    // choose_vehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
-    std::string choose_vehicle(std::string option1, std::string option2) {
-        // TODO: Return the final decision in a sentence.
-        return "not yet implemented";
+    // choose_vehicle recommends a vehicle for selection. It always recommends the vehicle 
+    // that comes first in lexicographical order.
+    string choose_vehicle(const string &option1, const string &option2) noexcept {
+        return min(option1, option2) + " is clearly the better choice.";
     }
-
     // calculate_resell_price calculates how much a vehicle can resell for at a certain age.
-    double calculate_resell_price(double original_price, double age) {
-        // TODO: Return the age-corrected resell price.
-        return 0.0;
+    auto calculate_resell_price(const double& original_price, 
+                                const double& age) noexcept -> double {
+        double discount { 1.0 };
+        if (age < 3) discount = 0.8;
+        else if (age < 10) discount = 0.7;
+        else discount = 0.5;
+        return original_price * discount;
     }
 
 }  // namespace vehicle_purchase
