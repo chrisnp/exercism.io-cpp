679d121e2c7556c1e8d2f0c4894f79b7d246c221
Invigo <invigo.devel@gmail.com>
2023-11-01 02:29:33 +0200

    Refactor vehicle_purchase.h

--- vehicle-purchase/vehicle_purchase.h
+++ vehicle-purchase/vehicle_purchase.h
@@ -1,11 +1,19 @@
 #pragma once
-
 #include <string>
 
 namespace vehicle_purchase {
+    using string = std::string;
 
-    bool needs_license(std::string kind);
-    std::string choose_vehicle(std::string option1, std::string option2);
-    double calculate_resell_price(double original_price, double age);
+    template<typename T> 
+    constexpr auto min(T &x, T &y) noexcept { return x < y ? x : y; }; 
+
+    auto needs_license(const string&) noexcept -> bool;
+    auto choose_vehicle(const string&, const string&) noexcept -> string;
+    auto calculate_resell_price(const double&, const double&) noexcept -> double;
 
 }  // namespace vehicle_purchase
+
+
+#ifndef EXERCISM_RUN_ALL_TESTS
+#define EXERCISM_RUN_ALL_TESTS
+#endif
