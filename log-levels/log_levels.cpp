8f827930c9b3bd4b0122dba97a59b22ad2539991
Invigo <invigo.devel@gmail.com>
2023-11-02 03:55:58 +0200

    Refactor log-levels

--- log-levels/log_levels.cpp
+++ log-levels/log_levels.cpp
@@ -1,19 +1,20 @@
 #include <string>
 
 namespace log_line {
+    using string = std::string;
 
     [[nodiscard]]
-    auto message(const std::string &line) noexcept -> std::string {
+    auto message(const string &line) noexcept -> string {
         int msg_start = line.find(':') + 2;
         return line.substr(msg_start);
     }
     [[nodiscard]]
-    auto log_level(const std::string &line) noexcept -> std::string {
+    auto log_level(const string &line) noexcept -> string {
         int log_level_start = line.find("[") + 1;
         int log_level_end = line.find("]:") - 1;
         return line.substr(log_level_start, log_level_end);
     }
-    auto reformat(const std::string &line) noexcept -> std::string {
+    auto reformat(const string &line) noexcept -> string {
         std::string level = "(" + log_level(line) + ")";
         return message(line) + " " + level;
     }
