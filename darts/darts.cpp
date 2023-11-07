824ace3cf596eaa2f20092f504a858394c9c7a6a
Invigo <invigo.devel@gmail.com>
2023-11-07 23:53:05 +0200

    Update darts.cpp

--- darts/darts.cpp
+++ darts/darts.cpp
@@ -1,5 +1,12 @@
 #include "darts.h"
+#include <cmath>
 
-namespace darts {
-
-} // namespace darts
+unsigned int darts::score(const double &x, const double &y) noexcept {
+    
+    unsigned int points { 0 };
+    double radius const = std::hypot(x, y);
+    if (radius <= 10.f) ++points;
+    if (radius <= 5.0f) ++++++++points; 
+    if (radius <= 1.0f) ++++++++++points;
+    return points; 
+}
