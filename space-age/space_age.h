1e5788b1404d6229c405772dd12738a325efe46b
Invigo <invigo.devel@gmail.com>
2023-11-07 23:47:30 +0200

    Refactor space_age.h

--- space-age/space_age.h
+++ space-age/space_age.h
@@ -1,24 +1,24 @@
 #pragma once
-
 #include <cstdint>
 
 namespace space_age {
 
 class space_age {
-
-    uint_fast64_t age;
-
+    
 public:
     explicit space_age(const uint_fast64_t);
-    uint_fast64_t seconds() const;
-    float on_mercury() const noexcept;
-    float on_venus() const noexcept;
-    float on_earth() const noexcept;
-    float on_mars() const noexcept;
-    float on_jupiter() const noexcept;
-    float on_saturn() const noexcept;
-    float on_uranus() const noexcept;
-    float on_neptune() const noexcept;
+    uint_fast64_t seconds() const noexcept;
+    auto on_mercury() const noexcept -> float;
+    auto on_venus() const noexcept -> float;
+    auto on_earth() const noexcept -> float;
+    auto on_mars() const noexcept -> float;
+    auto on_jupiter() const noexcept -> float;
+    auto on_saturn() const noexcept -> float;
+    auto on_uranus() const noexcept -> float;
+    auto on_neptune() const noexcept -> float;
+
+private:
+    uint_fast64_t const age;
 
 };
 } // namespace space_age
