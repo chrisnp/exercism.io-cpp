b5b411e2f0cec4e572e237a096e4e6f1c5f282d3
Invigo <invigo.devel@gmail.com>
2023-11-15 03:22:53 +0200

    Refactor allergies.cpp

--- allergies/allergies.cpp
+++ allergies/allergies.cpp
@@ -1,15 +1,15 @@
 #include "allergies.h"
 
-allergies::allergy_test::allergy_test(unsigned short score) {
+allergies::allergy_test::allergy_test(allergies::ushort score) : score(score) {
     score &= 0xff;
     for (int i = 0; i < static_cast<int>(ALLERGENS.size()); i++) 
-        if (1 << i & score) allergies.insert(ALLERGENS.at(i));
+       if (1 << i & score) allergies.insert(ALLERGENS.at(i)); 
 }
 
-bool allergies::allergy_test::is_allergic_to(const std::string &allergen) {
+bool allergies::allergy_test::is_allergic_to(const std::string &allergen) noexcept {
     return allergies.count(allergen) != 0;
 }
 
-std::unordered_set<std::string> allergies::allergy_test::get_allergies() const {
+allergies::allergies_t allergies::allergy_test::get_allergies() const {
     return allergies::allergy_test::allergies;
 }
