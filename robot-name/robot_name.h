1273a9c3a9d9a0ba7df91c7368f492600986df93
Invigo <invigo.devel@gmail.com>
2023-11-17 22:12:19 +0200

    robot_name.h - 1st iteration passing all tests

--- robot-name/robot_name.h
+++ robot-name/robot_name.h
@@ -1,8 +1,21 @@
-#if !defined(ROBOT_NAME_H)
-#define ROBOT_NAME_H
+#pragma once
+#include <string>
 
 namespace robot_name {
 
+class robot {
+
+public:
+    explicit robot();
+    auto reset() -> void;
+    auto name() const noexcept -> std::string const&;
+private:
+    std::string _name;
+};
+
 }  // namespace robot_name
 
-#endif // ROBOT_NAME_H
+
+#ifndef EXERCISM_RUN_ALL_TESTS
+#define EXERCISM_RUN_ALL_TESTS
+#endif
