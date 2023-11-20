b9b484bbcf4976fc474e7a8df1459b77f0a738ad
Invigo <invigo.devel@gmail.com>
2023-11-20 03:22:52 +0200

    Update triangle.cpp

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -1,5 +1,6 @@
 #include "triangle.h"
 #include <set>
+#include <stdexcept>
 
 triangle::flavor triangle::kind(double s1, double s2, double s3) {
     std::vector<double> sides {s1, s2, s3};
@@ -15,19 +16,19 @@ triangle::flavor triangle::kind(double s1, double s2, double s3) {
     return static_cast<triangle::flavor>(uniq_sides - 1);
 }
 
-double doubles::minimum(std::vector<double>& vecd) {
+double doubles::minimum(std::vector<double>& vecd) noexcept {
     double min {vecd[0]};
     for (double &f : vecd) if (f <= min) min = f;
     return min;
 }
 
-double doubles::maximum(std::vector<double>& vecd) {
+double doubles::maximum(std::vector<double>& vecd) noexcept {
     double max {vecd[0]};
     for (double &f : vecd) if (f >= max) max = f;
     return max;
 }
 
-double doubles::kahanSum(std::vector<double>& vecd) {
+double doubles::kahanSum(std::vector<double>& vecd) noexcept {
     double sum {0.0}; double e {0.0};
     for(double &f : vecd) {
         double y {f - e}; double t {sum + y};
