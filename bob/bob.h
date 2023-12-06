6420d2fda457dc1c6b36c17cb78c35bf167f0015
Invigo <invigo.devel@gmail.com>
2023-12-06 22:04:52 +0200

    Update bob.h

--- bob/bob.h
+++ bob/bob.h
@@ -7,14 +7,16 @@ namespace bob {
     static const std::regex shout {"^[^a-z]*[A-Z][^a-z]*$"};
     static const std::regex question {"\\?[^a-z]*$"};
     static const std::regex silence {"^\\W*$"};
-    
-    static const std::string RESPONSE_EMPHATIC { "Calm down, I know what I'm doing!" };
+
+    static const std::string RESPONSE_EMPHATIC { 
+        "Calm down, I know what I'm doing!" 
+    };
     static const std::string RESPONSE_SHOUTING { "Whoa, chill out!" };
     static const std::string RESPONSE_QUESTION { "Sure." };
     static const std::string RESPONSE_SILENCE  { "Fine. Be that way!" };
     static const std::string RESPONSE_DEFAULT  { "Whatever." };
     
-    std::string hey(const std::string &);
+    auto hey(const std::string&) noexcept -> std::string;
 }  // namespace bob
 
 #ifndef EXERCISM_RUN_ALL_TESTS
