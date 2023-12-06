129880c4c2c949e7e19f93be6dee46a5190d135e
Invigo <invigo.devel@gmail.com>
2023-12-06 22:04:57 +0200

    Update bob.cpp

--- bob/bob.cpp
+++ bob/bob.cpp
@@ -2,9 +2,8 @@
 
 using namespace std;
 
-string bob::hey(const string &query) {
-    return  regex_search(query, shout) && 
-            regex_search(query, question) 
+string bob::hey(const string &query) noexcept {
+    return  regex_search(query, shout) && regex_search(query, question) 
             ? RESPONSE_EMPHATIC 
             : regex_search(query, shout)    
             ? RESPONSE_SHOUTING 
