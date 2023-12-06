eafe45e385b99aec16c644d01840e0e4aa574307
Invigo <invigo.devel@gmail.com>
2023-12-06 22:01:48 +0200

    Reformat atbash_cipher.cpp

--- atbash-cipher/atbash_cipher.cpp
+++ atbash-cipher/atbash_cipher.cpp
@@ -8,12 +8,9 @@ std::string atbash_cipher::encode(const std::string &plain) noexcept {
         char c = tolower(ch);
         if( !(atbash_cipher::alpha(c) || atbash_cipher::digit(c)) ) 
             continue; 
-        if( index > 0 && index % 5 == 0) 
-            ciphertext.push_back(' ');
-        if( atbash_cipher::digit(c) ) 
-            ciphertext.push_back(c);
-        if( atbash_cipher::alpha(c) ) 
-            ciphertext.push_back('z' - c + 'a');        
+        if( index > 0 && index % 5 == 0) ciphertext.push_back(' ');
+        if( atbash_cipher::digit(c) ) ciphertext.push_back(c);
+        if( atbash_cipher::alpha(c) ) ciphertext.push_back('z' - c + 'a');
         ++index;
     }
     return ciphertext;
@@ -23,10 +20,8 @@ std::string atbash_cipher::decode(const std::string &cipher) {
     std::string plaintext {};
     for( auto &c: cipher ) {
         if( c == ' ') continue;
-        if( atbash_cipher::digit(c) ) 
-            plaintext.push_back(c);
-        if( atbash_cipher::alpha(c) ) 
-            plaintext.push_back('a' - c + 'z');
+        if( atbash_cipher::digit(c) ) plaintext.push_back(c);
+        if( atbash_cipher::alpha(c) ) plaintext.push_back('a' - c + 'z');
         if( !atbash_cipher::alpha(c) && !atbash_cipher::digit(c) )
             Catch::throw_domain_error("Bad cipher");
     }
