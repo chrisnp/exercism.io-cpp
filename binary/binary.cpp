b7c610b552f01896685845bcc92ed57cd01038ae
Invigo <invigo.devel@gmail.com>
2023-12-08 13:49:53 +0200

    Refactor binary.cpp

--- binary/binary.cpp
+++ binary/binary.cpp
@@ -3,11 +3,13 @@
 
 using string = std::string;
 
-long binary::convert (string binstr) noexcept {
-    for (auto& d: binstr) if (!isdigit(d)) return 0;
-    auto binary {stoll(binstr)}; 
+long binary::convert (const string& binstr) noexcept {
+    for ( auto&& d: binstr ) if ( !isdigit(d) ) return 0;
+    
+    auto binary { stoll(binstr, static_cast<size_t>(0), 10) }; 
     long decimal { 0 }, position { 0 };
-    while (binary != 0) {
+    
+    while ( binary != 0 ) {
         decimal += (binary % 10) * pow(2, position);
         position++;
         binary /= 10;
