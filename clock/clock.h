a49323d11e5eaf37f2b09cd8a74160e4ca1c808c
Invigo <invigo.devel@gmail.com>
2023-12-08 13:50:20 +0200

    Refactor clock.h

--- clock/clock.h
+++ clock/clock.h
@@ -2,17 +2,20 @@
 #include <string>
 
 namespace date_independent {
+
 class clock {
+
 private:
     const int minutes;
+
 public:
     explicit clock(int minutes);
     static clock at(int hours, int minutes = 0);
+    explicit operator std::string() const;
     clock plus(int ms) const noexcept;
     clock minus(int ms) const noexcept;
-    bool operator==(const clock &other) const noexcept;
-    bool operator!=(const clock &other) const noexcept;
-    explicit operator std::string() const;
+    auto operator==(const clock &other) const noexcept -> bool;
+    auto operator!=(const clock &other) const noexcept -> bool;
 };
 
 }  // namespace date_independent
