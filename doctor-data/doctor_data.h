e203b2a686c5a545fab0ef56ae91887081bdd39e
Invigo <invigo.devel@gmail.com>
2024-01-05 23:43:12 +0200

    Update doctor_data.h

--- doctor-data/doctor_data.h
+++ doctor-data/doctor_data.h
@@ -21,10 +21,16 @@ using string = std::string;
 
 struct Vessel {
     Vessel(string name, int generation, 
-           star_map::System current_system = star_map::System::Sol);
-    auto replicate(string name) const -> Vessel;
+           star_map::System current_system = star_map::System::Sol) {};
+    auto replicate(string new_name) const -> Vessel;
     auto make_booster() -> void;
     auto shoot_booster() -> bool;
+    string name {};
+    int genaration {};
+    star_map::System current_system {};
+    int boosters { 0 };
 }
+
+auto get_older_bob(Vessel)
 
 } // namespace heaven
