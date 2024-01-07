b03416ceb9d8ba66eef7ba0e1c596bb5bf2101cf
Invigo <invigo.devel@gmail.com>
2024-01-08 00:21:15 +0200

    doctor_data.h - 1st passing iteration

--- doctor-data/doctor_data.h
+++ doctor-data/doctor_data.h
@@ -17,21 +17,26 @@ enum class System {
 
 namespace heaven {
 
-using string = std::string;
+using std::string;
 
 struct Vessel {
     Vessel(string name, int generation, 
            star_map::System current_system = star_map::System::Sol);
     auto replicate(string new_name) const -> Vessel;
-    auto make_booster() -> void;
-    auto shoot_booster() -> bool;
+    auto make_buster() -> void;
+    auto shoot_buster() -> bool;
     string name {};
     int generation {};
     star_map::System current_system {};
-    int boosters { 0 };
-}
+    int busters { 0 };
+};
 
 auto get_older_bob(Vessel const&, Vessel const&) noexcept -> string;
 auto in_the_same_system(Vessel const&, Vessel const&) noexcept -> bool;
 
 } // namespace heaven
+
+
+#ifndef EXERCISM_RUN_ALL_TESTS
+#define EXERCISM_RUN_ALL_TESTS
+#endif
