25a78cc3773419f55e59d9690748ee1e03528f1e
Invigo <invigo.devel@gmail.com>
2024-02-09 11:04:01 +0200

    Create eliuds_eggs.h

---
+++ eliuds-eggs/eliuds_eggs.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace chicken_coop {
+
+[[gnu::pure]]    
+inline auto positions_to_quantity(int displayed) noexcept -> unsigned int {
+    if (displayed <= 0) return 0;
+    return displayed % 2 + positions_to_quantity(displayed / 2);
+}
+    
+}  // namespace chicken_coop
+
+
+#ifndef EXERCISM_RUN_ALL_TESTS
+#define EXERCISM_RUN_ALL_TESTS
+#endif
