dd047bda22294f14c7ec41e4dcb93a2f642d4149
Invigo <invigo.devel@gmail.com>
2024-02-12 22:23:59 +0200

    Update resistor_color.cpp

--- resistor-color/resistor_color.cpp
+++ resistor-color/resistor_color.cpp
@@ -1,5 +1,19 @@
 #include "resistor_color.h"
 
-namespace resistor_color {
+using std::vector;
+using std::string;
 
-}  // namespace resistor_color
+vector<string> resistor_color::colors() noexcept {
+    return resistor_color::_colors;
+}
+
+int resistor_color::color_code(const string &color) {
+    int value {0};
+    vector<string> colors = resistor_color::colors();
+    for (int i {0}; i < 10; i++) {
+        if (color == colors.at(i)) {
+            value += i;
+        }
+    }
+    return value;
+}
