bcbe9323d78634ee8067c9666a08149acb477f21
Invigo <invigo.devel@gmail.com>
2024-02-20 23:19:29 +0200

    Create rotational_cipher.h

---
+++ rotational-cipher/rotational_cipher.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+namespace rotational_cipher {
+
+using std::string;
+
+static const char ALPHA_LOWER = 'a';
+static const char ALPHA_UPPER = 'A';
+static const int  ALPHABET = 26;
+
+auto rotate(const string&, int) noexcept -> string;
+
+}  // namespace rotational_cipher
+
+#ifndef EXERCISM_RUN_ALL_TESTS
+#define EXERCISM_RUN_ALL_TESTS
+#endif
