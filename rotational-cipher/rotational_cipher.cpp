3874dc46bbd849400098ff77d22363424242e2ba
Invigo <invigo.devel@gmail.com>
2024-02-21 01:00:04 +0200

    Update rotational_cipher.cpp

--- rotational-cipher/rotational_cipher.cpp
+++ rotational-cipher/rotational_cipher.cpp
@@ -1,5 +1,14 @@
 #include "rotational_cipher.h"
 
-namespace rotational_cipher {
-
-}  // namespace rotational_cipher
+std::string 
+rotational_cipher::rotate(const string &plaintext, int shift) noexcept {
+    string enciphered {};
+    shift %= ALPHABET;
+    for (auto ch: plaintext) {
+        auto const alpha = std::islower(ch) ? rotational_cipher::ALPHA_LOWER 
+                                            : rotational_cipher::ALPHA_UPPER;
+        if (std::isalpha(ch)) ch = alpha + (ch - alpha + shift) % ALPHABET;
+        enciphered += ch;
+    }
+    return { enciphered.begin(), enciphered.end() };
+}
