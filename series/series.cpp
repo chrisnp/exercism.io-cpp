d14eefa23c9d7f3eb1e0f672d230d23e11359fe4
Invigo <invigo.devel@gmail.com>
2024-02-23 16:39:30 +0200

    Refactor series.cpp

--- series/series.cpp
+++ series/series.cpp
@@ -1,17 +1,18 @@
 #include "series.h"
 #include <stdexcept>
 
-using namespace std;
+using std::string;
+using std::domain_error;
 
-vector<string> series::slice(string const &input, uint_t slice_size) {
+series::slices_t series::slice(string const &input, luint_t slice_size) {
     if ( slice_size <= 0)
         throw domain_error("enigmatic error for not actually slicing");
-    uint_t input_size = input.length();
+    luint_t input_size { input.length() };
     if ( input_size < slice_size ) 
         throw domain_error("cannot slice a string shorter than the slicesize");
-    vector<string> slices {};
-    for ( uint_t i = 0; i <= input_size - slice_size; ++i ) {
+    series::slices_t slices {};
+    for ( luint_t i = 0UL; i <= input_size - slice_size; ++i ) {
         slices.push_back(input.substr(i, slice_size));
     }  
-    return slices;
+    return { slices.begin(), slices.end() };
 }
