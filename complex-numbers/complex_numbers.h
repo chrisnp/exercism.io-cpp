23fab7617e85e28ca3c67910379e70203c5bff09
Invigo <invigo.devel@gmail.com>
2024-03-28 23:44:57 +0200

    Create complex_numbers.h

--- complex-numbers/complex_numbers.h
+++ complex-numbers/complex_numbers.h
@@ -2,6 +2,23 @@
 
 namespace complex_numbers {
 
+class Complex {
+public:
+    Complex(double real, double imag);
+    auto real() const -> double;
+    auto imag() const -> double;
+    auto abs() const -> double;
+    auto conj() const -> Complex;
+    auto exp() const -> Complex;
+    auto operator*(const Complex &that) const -> Complex;
+    auto operator/(const Complex &that) const -> Complex;
+    auto operator+(const Complex &that) const -> Complex;
+    auto operator-(const Complex &that) const -> Complex;
+private:
+    double _Real;
+    double _Imag;
+};
+
 }  // namespace complex_numbers
 
 
