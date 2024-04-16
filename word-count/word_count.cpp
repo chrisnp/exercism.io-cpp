4ddd2f08810ee7effcef0a38cfa2e8c70fb982c6
Invigo <invigo.devel@gmail.com>
2024-04-16 23:41:15 +0300

    Update word_count.cpp

--- word-count/word_count.cpp
+++ word-count/word_count.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-constexpr auto delim( const char &c ) noexcept -> bool {
+constexpr auto delim( const char &c ) -> bool {
     return c == ' ' || c == ',' || c == '\n';
 }
 
@@ -13,7 +13,7 @@ const map<string, int> word_count::words( const string &text ) {
     for ( unsigned int i = 0; i < text.length(); i++ ) {
         auto &c = text[i];
         if ( delim(c) && !wordchars.empty() ) {
-            wordcount[string(wordchars.begin(), wordchars.end())] += 1;
+            wordcount[string(wordchars.begin(), wordchars.end())]++;
             wordchars = vector<char>();
         } 
         else if ( (c == '\'' && isalnum(text[i - 1]) && isalnum(text[i + 1])) 
@@ -23,6 +23,6 @@ const map<string, int> word_count::words( const string &text ) {
         else continue;
     }
     if ( !wordchars.empty() ) 
-        wordcount[string(wordchars.begin(), wordchars.end())] += 1;
+        wordcount[string(wordchars.begin(), wordchars.end())]++;
     return wordcount;
 }
