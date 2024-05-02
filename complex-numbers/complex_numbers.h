85fc12dc24337cf0ba11681bd8d9c6f4111f9914
Invigo <invigo.devel@gmail.com>
2024-05-02 23:23:05 +0300

    Update complex_numbers.h

--- complex-numbers/complex_numbers.h
+++ complex-numbers/complex_numbers.h
@@ -1,8 +1,27 @@
-#if !defined(COMPLEX_NUMBERS_H)
-#define COMPLEX_NUMBERS_H
+#pragma once
 
 namespace complex_numbers {
 
+class Complex {
+public:
+    Complex(double real, double imag);
+    [[nodiscard]] auto real() const -> double;
+    [[nodiscard]] auto imag() const -> double;
+    [[nodiscard]] auto abs() const -> double;
+    [[nodiscard]] auto conj() const -> Complex;
+    [[nodiscard]] auto exp() const -> Complex;
+private:
+    double _Real;
+    double _Imag;
+};
+
+Complex operator+(const Complex&, const Complex&);
+Complex operator-(const Complex&, const Complex&);
+Complex operator*(const Complex&, const Complex&);
+Complex operator/(const Complex&, const Complex&);
 }  // namespace complex_numbers
 
-#endif  // COMPLEX_NUMBERS_H
+
+#ifndef EXERCISM_RUN_ALL_TESTS
+#define EXERCISM_RUN_ALL_TESTS
+#endif
