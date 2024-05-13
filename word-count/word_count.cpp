c9d8342809012708ccc24fd68c67bb8d2eb6c553
Invigo <invigo.devel@gmail.com>
2024-05-13 03:10:24 +0300

    Refactor word_count.cpp

--- word-count/word_count.cpp
+++ word-count/word_count.cpp
@@ -1,27 +1,26 @@
 #include "word_count.h"
 #include <vector>
 
-const word_count::words_map_t word_count::words( const std::string &text ) {
-    constexpr auto word_delimiter = [&]( const char &c ) noexcept -> bool {
+using std::string;
+using word_count::words_map_t;
+
+const words_map_t word_count::words( const string &text ) {
+    constexpr auto word_delimiter = [&](const char c) noexcept -> bool {
         return c == ' ' || c == ',' || c == '\n';
     };
-    word_count::words_map_t wordcount {};
+    words_map_t wordcount {};
     std::vector<char> wordchars {};
     for ( unsigned int i = 0; i < text.length(); i++ ) {
         const auto &c = static_cast<char>(text[i]);
-        if ( isalnum(c) ) {
-            wordchars.push_back(std::tolower(c));
+        if ( isalnum(c) ) wordchars.push_back(std::tolower(c));
+        if ( !wordchars.empty() ) { 
+            if ( c == '\'' && isalnum(text[i + 1]) ) wordchars.push_back(c);
+            if ( word_delimiter(c) ) {
+                ++wordcount[string(wordchars.begin(), wordchars.end())];
+                wordchars.clear();
+            }
         }
-        else if ( c == '\'' && !wordchars.empty() && isalnum(text[i + 1]) ) {
-            wordchars.push_back(std::tolower(c));
-        }
-        else if ( word_delimiter(c) && !wordchars.empty() ) {
-            wordcount[std::string(wordchars.begin(), wordchars.end())] += 1;
-            wordchars = std::vector<char>();
-        }  
-        else continue;
     }
-    if ( !wordchars.empty() ) 
-        wordcount[std::string(wordchars.begin(), wordchars.end())] += 1;
+    wordcount[string(wordchars.begin(), wordchars.end())] += 1;
     return wordcount;
 }
