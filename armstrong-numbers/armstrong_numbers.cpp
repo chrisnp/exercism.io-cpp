808563040100f801cfa09df1d7e1ecad77a36535
Invigo <invigo.devel@gmail.com>
2024-06-05 22:03:33 +0300

    Reformat armstrong_numbers.cpp

--- armstrong-numbers/armstrong_numbers.cpp
+++ armstrong-numbers/armstrong_numbers.cpp
@@ -4,13 +4,10 @@
 
 bool armstrong_numbers::is_armstrong_number(const int &num) noexcept {
     const std::string digits { std::to_string(num) };
-
-    int num_digits { static_cast<int>(digits.length()) }, 
-        sum_digits_raised { 0 };
-
+    int sum_digits_raised { 0 }, 
+        num_digits { static_cast<int>(digits.length()) };
     for (auto digit : digits) {
         sum_digits_raised += pow(digit - '0', num_digits); 
     }
-
     return num >= 0 && sum_digits_raised == num;
 }
