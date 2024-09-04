f88952435b9f4385f4b55a9f4221a2ab9474c138
Invigo <invigo.devel@gmail.com>
2024-09-04 16:24:06 +0300

    Refactor circular_buffer.h

--- circular-buffer/circular_buffer.h
+++ circular-buffer/circular_buffer.h
@@ -3,36 +3,54 @@
 #include <stdexcept>
 
 namespace circular_buffer {
-
-using size_t = std::size_t;
 using std::string;
 
 template <typename T> struct circular_buffer {
 
-    explicit circular_buffer(size_t);
-    ~circular_buffer();
-    [[nodiscard]] auto const read() -> T&;
-    auto write(const T& item) -> void;
-    auto overwrite(const T& item) -> void;
-    auto clear() -> void;
+    explicit circular_buffer(size_t size)
+    : _read_idx {0}, 
+      _write_idx {0}, 
+      _capacity {size}, 
+      _buffer {new T[size]}
+    { clear(); };
+
+    [[nodiscard]] auto const read() -> T& {
+        if (current_size() == 0) 
+            throw std::domain_error("Buffer is empty.");
+        size_t curr_read = _read_idx;
+        advance(_read_idx);
+        return _buffer[curr_read];   
+    }
+    auto write(const T& item) -> void {
+        if (current_size() == capacity())
+            throw std::domain_error("Buffer is full.");
+        _buffer[_write_idx] = item;
+        advance(_write_idx);
+        if (_write_idx == _read_idx) advance(_read_idx);
+    }
+    auto overwrite(const T& item) -> void {
+        if (current_size() >= capacity()) advance(_read_idx);
+        write(item);
+    }
+    auto clear() -> void { _write_idx = _read_idx = 0; }
+
+    ~circular_buffer() { delete[] _buffer; };
 
 protected:
-    auto capacity() const -> size_t;
-    auto current_size() const -> size_t;
-    // auto empty() const -> bool { return _widx == _ridx; }
-    
+    auto capacity() const -> size_t { return _capacity; }
+    auto current_size() const -> size_t {
+        return _write_idx >= _read_idx 
+               ? _write_idx - _read_idx 
+               : capacity() - (_read_idx - _write_idx - 1);
+    }
+    auto empty() const -> bool { return _write_idx == _read_idx; }
+
 private:
-    size_t _ridx, _widx;
+    size_t _read_idx, _write_idx;
     size_t _capacity;
     T *_buffer;
-    auto advance(size_t& val) -> void;
-};
-
-/**
-    And two needed declarations, to keep the linker happy.
-*/
-template struct circular_buffer<int>;
-template struct circular_buffer<std::string>;
+    auto advance(size_t& val) -> void { ++val %= _capacity + 1; }
+}; // struct circular_buffer
 
 }  // namespace circular_buffer
 
