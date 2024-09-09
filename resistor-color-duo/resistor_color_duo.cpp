60135b1947e2f095ee939b6cd5a9f6d7dd56390b
Invigo <invigo.devel@gmail.com>
2024-09-09 23:47:41 +0300

    Update resistor_color_duo.cpp

--- resistor-color-duo/resistor_color_duo.cpp
+++ resistor-color-duo/resistor_color_duo.cpp
@@ -1,5 +1,15 @@
 #include "resistor_color_duo.h"
 
-namespace resistor_color_duo {
+using std::string;
+using std::vector;
 
-}  // namespace resistor_color_duo
+auto color_code(const string& color) -> int {
+    for (size_t i {}; i <= resistor_color_duo::colors.size(); ++i)
+        if(resistor_color_duo::colors.at(i) == color)            
+            return i;
+    return -1;
+}
+
+int resistor_color_duo::value(const vector<string>& colors) {
+    return 10 * color_code(colors.at(0)) + color_code(colors.at(1));
+}
