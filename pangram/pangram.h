b3ed511862b0af78209c6c3ee87805b604a6c70d
Invigo <invigo.devel@gmail.com>
2024-09-27 00:02:04 +0300

    Update pangram.h

--- pangram/pangram.h
+++ pangram/pangram.h
@@ -6,6 +6,10 @@ namespace pangram {
 
 constexpr int ALPHABET_SIZE { 26 };
 
+constexpr auto letter_idx(char ch) noexcept -> int {
+    return tolower(ch) - 'a';
+}
+
 bool is_pangram(const std::string &) noexcept;
 
 }  // namespace pangram
