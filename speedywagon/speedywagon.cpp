f9b446a50d7c03b76bf71ab7aa0c375a8a412b50
Invigo <invigo.devel@gmail.com>
2025-08-29 20:36:12 +0300

    Update speedywagon.cpp

--- speedywagon/speedywagon.cpp
+++ speedywagon/speedywagon.cpp
@@ -3,9 +3,28 @@
 namespace speedywagon {
 
 // Enter your code below:
+bool connection_check(const pillar_men_sensor* sensor) {
+    return sensor != nullptr;
+}
+
+int activity_counter(const pillar_men_sensor* sensors, int sensor_count) {
+    auto count = int{};
+    for(auto i = int{ 0 }; i < sensor_count; ++i) {
+      count += sensors[i].activity;
+    }
+    return count;
+}
+
+bool alarm_control(const pillar_men_sensor* sensor) {
+    return sensor && sensor->activity > 0;
+}
+
+bool uv_alarm(const pillar_men_sensor* sensor) {
+    return sensor && (uv_light_heuristic(&sensor->data) > sensor->activity);
+}
 
 // Please don't change the interface of the uv_light_heuristic function
-int uv_light_heuristic(std::vector<int>* data_array) {
+int uv_light_heuristic(const std::vector<int>* data_array) {
     double avg{};
     for (auto element : *data_array) {
         avg += element;
@@ -19,3 +38,4 @@ int uv_light_heuristic(std::vector<int>* data_array) {
 }
 
 }  // namespace speedywagon
+
